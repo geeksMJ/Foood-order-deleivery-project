caf8a2ba2627761c0a69a5a4f73e46e9d6601dd1
geeksMJ <mangeshpandey888dd@gmail.com>
2024-09-02 02:56:49 +0530

    food order deleivery project

---
+++ frontend/src/components/layouts/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+      
+    <footer>
+        <p className='text-center'>
+              Food Delivery Website- 2024-2025. All Rights Reserved By WSA
+        </p>
+    </footer>
+  )
+}
